b52b5d54f93e57d2ecb3de006f5f0f3a6ed50de2
Mindeveloped <6187869@qq.com>
2024-11-07 15:01:27 +0800

    Migration
    Migrated files from /melody/ to /
    Also added matrix template as it's commonly used for me

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define F(i,x,y) for (int i=(x);i<=(y);i++)
+#define R(i,x,y) for (int i=(x);i>=(y);i--)
+#define p2i pair<int,int>
+#define ll long long
+#define fi first
+#define se second
+#define pub push_back
+#define ins insert
+#define puf push_front
+#define vec vector
+#define umap unordered_map
+#define uset unordered_set
+#define popf pop_front
+#define popb pop_back
+#define nb(x) (1<<((x)-1))
+#define iosoptim ios::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+const bool MT = 0;
+
+void solve(){
+	// Insert your code here
+}
+int main (){
+	int testcases=1;
+	if(MT) cin>>testcases;
+	while(testcases--){
+		solve();
+	}
+	return 0;
+}
---
+++ utilities/matrix.cpp
@@ -0,0 +1,31 @@
+struct mat {
+    const int DIM=2;
+    int a[DIM][DIM];
+    mat () {
+        memset(a,0,sizeof(a));
+    }
+    mat (const mat &b) {
+        memcpy(a,b.a,sizeof(b.a));
+    }
+    mat operator + (const mat&b) const {
+        mat c;
+        F(i,0,DIM-1){
+            F(j,0,DIM-1){
+                c.a[i][j]=a[i][j]+b.a[i][j];
+            }
+        }
+        return c;
+    }
+    mat operator * (const mat&b) const {
+        mat c;
+        F(i,0,DIM-1){
+            F(j,0,DIM-1){
+                c.a[i][j]=0;
+                F(k,0,DIM-1){
+                    c.a[i][j]+=a[i][k]*b.a[k][j];
+                }
+            }
+        }
+        return c;
+    }
+};
